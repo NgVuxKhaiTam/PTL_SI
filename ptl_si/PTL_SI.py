cd1ced0a5d1b3023573b1be61696e80285022856
Nguyen Vu Khai Tam <72638887+NgVuxKhaiTam@users.noreply.github.com>
2025-06-19 09:02:14 +0700

    Update PTL_SI.py

--- ptl_si/PTL_SI.py
+++ ptl_si/PTL_SI.py
@@ -1,84 +1,8 @@
-
-    
-
 import utils
 import transfer_learning_hdr
 import sub_prob
 import random
 import numpy as np
-
-
-def divide_and_conquer_TF_op(X, X0, a, b, Mobs, N, nT, K, p, B, Q, lambda_0, lambda_tilde, ak_weights, z_min, z_max):
-    z = z_min
-    a_tilde = np.concatenate([ak_weights[k] * np.ones(p) for k in range(K)] + [np.ones(p)]).reshape(-1, 1)
-    intervals = []
-    while z < z_max:
-        Yz = a + b*z
-        Yz = Yz.ravel()
-        Y0z = Q @ Yz
-        tz, wz, dz, bz = transfer_learning_hdr.TransFusion(X, Yz, X0, Y0z, B, N, p, K, lambda_0, lambda_tilde, ak_weights)
-
-        thetaO, SO, O, XO, Oc, XOc = utils.construct_thetaO_SO_O_XO_Oc_XOc(tz, X)
-        deltaL, SL, L, X0L, Lc, X0Lc = utils.construct_detlaL_SL_L_X0L_Lc_X0Lc(dz, X0)
-        betaM, M, SM, Mc = utils.construct_betaM_M_SM_Mc(bz)
-        phi_u, iota_u, xi_uv, zeta_uv = sub_prob.calculate_phi_iota_xi_zeta(X, SO, O, XO, X0, SL, L, X0L, p, B, Q, lambda_0, lambda_tilde, a_tilde, N, nT)
-        
-        # utils.check_KKT_theta(XO, XOc, Yz, O, Oc, thetaO, SO, lambda_0, a_tilde, N)
-        # utils.check_KKT_delta(X0L, X0Lc, Yz, L, Lc, deltaL, SL, phi_u, iota_u, lambda_tilde, nT)
-       
-        lu, ru = compute_Zu_2(SO, O, XO, Oc, XOc, a, b, lambda_0, a_tilde, N)
-
-        lv, rv = compute_Zv_2(SL, L, X0L, Lc, X0Lc, phi_u, iota_u, a, b, lambda_tilde, nT)
-
-        lt, rt = compute_Zt_2(M, SM, Mc, xi_uv, zeta_uv, a, b)
-
-        r = min(ru, rv, rt)
-        l = max(lu, lv, lt)
-        if r < l or r < z: 
-            print ('Err')
-            return []
-
-        if M == Mobs:
-            intervals.append((l, r))
-
-        z = r + 1e-4
-
-    return intervals
-
-
-def PTL_SI_TF_op(X0, Y0, XS_list, YS_list, lambda_0, lambda_tilde, ak_weights, SigmaS_list, Sigma0, z_min=-20, z_max=20):
-    K = len(YS_list)
-    nS = YS_list[0].shape[0]
-    nT = Y0.shape[0]
-    N = nS * K + nT
-    p = X0.shape[1]
-
-    X = utils.construct_X(XS_list, X0, p, K)
-    Y = np.concatenate(YS_list + [Y0])
-    B = utils.construct_B(K, p, nS, nT)
-    Sigma = utils.construct_Sigma(SigmaS_list, Sigma0)
-
-    theta_hat, w_hat, delta_hat, beta0_hat = transfer_learning_hdr.TransFusion(X, Y, X0, Y0, B, N, p, K, lambda_0, lambda_tilde, ak_weights)
-    M_obs = [i for i in range(p) if beta0_hat[i] != 0.0]
-
-    if len(M_obs) == 0:
-        return None
-    
-    X0M = X0[:, M_obs]
-    Q = utils.construct_Q(nT, N)
-
-    p_sel_list = []
-    
-    for j in M_obs:
-        etaj, etajTY = utils.construct_test_statistic(j, X0M, Y, M_obs, nT, N)
-        a, b = utils.calculate_a_b(etaj, Y, Sigma, N)
-        intervals = divide_and_conquer_TF_op(X, X0, a, b, M_obs, N, nT, K, p, B, Q, lambda_0, lambda_tilde, ak_weights, z_min, z_max)
-        pj_sel = utils.calculate_TN_p_value(intervals, etaj, etajTY, Sigma, 0)
-
-        p_sel_list.append((j, pj_sel))
-    
-    return p_sel_list
-
 
 
 def divide_and_conquer_TF(X, X0, a, b, Mobs, N, nT, K, p, B, Q, lambda_0, lambda_tilde, ak_weights, z_min, z_max):
